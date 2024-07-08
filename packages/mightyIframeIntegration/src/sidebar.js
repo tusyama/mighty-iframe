eac1745c27c120bf1784108678c89f445caae9cb
m.barabanschikov <m.barabanschikov@farmaimpex.ru>
2024-07-08 10:41:25 +0400

    added styles css

--- packages/mightyIframeIntegration/src/sidebar.js
+++ packages/mightyIframeIntegration/src/sidebar.js
@@ -4,35 +4,41 @@ class Sidebar {
   constructor() {
     this.currentSidebar = null;
     this.initializedTriggers = new Set();
+    this.mightySidebarId = 'mighty-course-sidebar';
+    this.mightyStyleId = 'mighty-sidebar-styles';
+
+    this.addStyles();
   }
 
-  createSidebar() {
-    const sidebar = document.createElement('div');
-    sidebar.id = 'course-sidebar';
-    sidebar.style.position = 'fixed';
-    sidebar.style.top = '0';
-    sidebar.style.right = '-400px'; // Начальная позиция за пределами экрана
-    sidebar.style.width = '400px';
-    sidebar.style.height = '100%';
-    sidebar.style.color = '#AFB4B8';
-    sidebar.style.background = 'radial-gradient(100% 60% at 100% 100%, rgba(98, 126, 234, 0.2) 0%, rgba(0, 0, 0, 0.02) 97.05%), rgb(28, 29, 38)';
-    sidebar.style.boxShadow = '-2px 0 5px rgba(0,0,0,0.5)';
-    sidebar.style.zIndex = '1000';
-    sidebar.style.display = 'flex';
-    sidebar.style.flexDirection = 'column';
-    sidebar.style.transition = 'right 0.3s ease, width 0.3s ease'; // Анимация выезжания и изменения ширины
+  addStyles() {
+    if (document.querySelector(this.mightyStyleId)) {
+      return;
+    }
 
-    const header = document.createElement('div');
-    header.style.display = 'flex';
-    header.style.justifyContent = 'flex-start';
-    header.style.alignItems = 'center';
-    header.style.padding = '10px';
-
-    // Создание кнопки
-    const createButton = (svgContent, onClick) => {
-      const button = document.createElement('div');
-      button.innerHTML = svgContent;
-      button.style.cssText = `
+    const style = document.createElement('style');
+    style.id = this.mightyStyleId;
+    style.textContent = `
+      #${this.mightySidebarId} {
+        position: fixed;
+        top: 0;
+        right: -400px; /* Initial position off-screen */
+        width: 400px;
+        height: 100%;
+        color: #AFB4B8;
+        background: radial-gradient(100% 60% at 100% 100%, rgba(98, 126, 234, 0.2) 0%, rgba(0, 0, 0, 0.02) 97.05%), rgb(28, 29, 38);
+        box-shadow: -2px 0 5px rgba(0,0,0,0.5);
+        z-index: 1000;
+        display: flex;
+        flex-direction: column;
+        transition: right 0.3s ease, width 0.3s ease; /* Animation for sliding out and width change */
+      }
+      #${this.mightySidebarId} .mighty-header-sidebar {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        padding: 10px;
+      }
+      #${this.mightySidebarId} .mighty-sidebar-button {
         display: flex;
         justify-content: center;
         align-items: center;
@@ -43,9 +49,43 @@ class Sidebar {
         transition: all 0.3s ease;
         cursor: pointer;
         background: transparent;
-      `;
-      button.onmouseover = () => button.style.background = 'rgba(255, 255, 255, 0.15)';
-      button.onmouseout = () => button.style.background = 'transparent';
+      }
+      #${this.mightySidebarId} .mighty-sidebar-button:hover {
+        background: rgba(255, 255, 255, 0.15);
+      }
+      #${this.mightySidebarId} iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+      }
+      @media (max-width: 600px) {
+        #${this.mightySidebarId} {
+          width: 100%;
+          right: -100%;
+        }
+      }
+    `;
+    document.head.appendChild(style);
+  }
+
+  createSidebar() {
+    const sidebar = document.createElement('div');
+    sidebar.id = this.mightySidebarId;
+
+    const header = document.createElement('div');
+    header.classList.add('mighty-header-sidebar');
+    header.style.display = 'flex';
+    header.style.justifyContent = 'flex-start';
+    header.style.alignItems = 'center';
+    header.style.padding = '10px';
+
+    // Создание кнопки
+    const createButton = (svgContent, onClick) => {
+      const button = document.createElement('div');
+      button.innerHTML = svgContent;
+      button.classList.add('mighty-sidebar-button');
+      // button.onmouseover = () => button.style.background = 'rgba(255, 255, 255, 0.15)';
+      // button.onmouseout = () => button.style.background = 'transparent';
       button.onclick = onClick;
       return button;
     };
@@ -73,10 +113,6 @@ class Sidebar {
     sidebar.appendChild(header);
 
     const iframe = document.createElement('iframe');
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.border = 'none';
-
     sidebar.appendChild(iframe);
 
     return { sidebar, iframe };
@@ -90,8 +126,8 @@ class Sidebar {
         setTimeout(() => {
           sidebar.style.right = '-400px'; // Анимация заезжания
           setTimeout(() => {
-            if (document.querySelector('#course-sidebar')) {
-              document.body.removeChild(document.querySelector('#course-sidebar'));
+            if (document.querySelector(`#${this.mightySidebarId}`)) {
+              document.body.removeChild(document.querySelector(`#${this.mightySidebarId}`));
             }
             this.currentSidebar = null; // Сброс глобальной переменной
             resolve();
@@ -100,8 +136,8 @@ class Sidebar {
       } else {
         sidebar.style.right = '-400px'; // Анимация заезжания
         setTimeout(() => {
-          if (document.querySelector('#course-sidebar')) {
-            document.body.removeChild(document.querySelector('#course-sidebar'));
+          if (document.querySelector(`#${this.mightySidebarId}`)) {
+            document.body.removeChild(document.querySelector(`#${this.mightySidebarId}`));
           }
           this.currentSidebar = null; // Сброс глобальной переменной
           resolve();
